ff517ca066753134acae849563bd668f411a0675
mckaymac <54556260+mckaymac@users.noreply.github.com>
2020-02-11 10:23:58 -0500

    Added count text and win text UI and scripting

--- McKay_RollABall/Assets/Scripts/BallController.cs
+++ McKay_RollABall/Assets/Scripts/BallController.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -28,6 +36,15 @@ public class BallController : MonoBehaviour
 
         if(other.gameObject.CompareTag("PickUp")){
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if(count >= 13){
+            winText.text = "You Win!";
         }
     }
 }
